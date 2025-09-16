833c766a5b06e30764990118a72299aa6c7c77fa
SeoyoungCho0625 <joyseoyoung@snu.ac.kr>
2025-09-16 17:10:12 +0900

    test

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -3,12 +3,28 @@ let face;
 
 function preload() {
   body = loadImage('body.png');
-  face = loadImage('1.png');
+
+  // get random face
+  let n = int(random(1, 5));
+  let imgName = n + ".png";  
+  face = loadImage(imgName);
 }
 
 function setup() {
   createCanvas(400, 400);
-  background('beige');
+
+  let button = createButton('🤗');
+  button.position(10, 10);
+
+  button.mousePressed(() => {
+    let n = int(random(1, 5));
+    let imgName = n + ".png";
+    face = loadImage(imgName);
+  });  
+}
+
+function draw() {
+  background("beige");
   image(body, 0, 0);
-  image(face, 132, 134);
+  image(face, 132, 134);  
 }
