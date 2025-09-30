76381df02c54577eb85b18af0bd03bf374b9b1c9
SeoyoungCho0625 <joyseoyoung@snu.ac.kr>
2025-09-30 15:12:03 +0900

    pattern06

--- pattern/sketch.js
+++ pattern/sketch.js
@@ -34,6 +34,16 @@ function draw() {
      rectMode(CENTER);
      rect(0, 0, sz, sz);
      pop();
+
+     push();
+     translate(width/2, height/2); // move origin to canvas center
+     rotate(random(TWO_PI));       // random rotation
+      const s = random(300, 500);   // side length
+      const h = (sqrt(3) / 2) * s;  // height of equilateral triangle
+      fill('cyan'); 
+      triangle(-s/2, h/3, s/2, h/3, 0, -2*h/3);
+      pop();
+
    }
  }
 }
