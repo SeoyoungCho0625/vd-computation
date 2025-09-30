a1089d28871437a7bf1daac2b755683812955b52
SeoyoungCho0625 <joyseoyoung@snu.ac.kr>
2025-09-30 14:33:14 +0900

    pattern02

--- pattern/sketch.js
+++ pattern/sketch.js
@@ -1,31 +1,45 @@
+const GRID = 2;
+const CELL = 1080 / GRID;
+
 function setup() {
-  createCanvas(1080, 1080);
-  noStroke();
-  noLoop();
+ createCanvas(1080, 1080);
+ noStroke();
+ // frameRate(2);
+ noLoop();
 }
 
-function draw(){
- background(2255,255,220);
- fill('orange');
- circle(width/2, height/2, random (300,500));
+function draw() {
+ background(255, 255, 220);
+ const cz = random(40, CELL);
+ const sz = random(40, CELL);
 
- translate(width/2, height/2);
- fill('pink');
- rectMode(CENTER);
- rect(0, 0, random (300,500));
+ for (let gy = 0; gy < GRID; gy++) {
 
- push();
-  blendMode(MULTIPLY);
-  fill('orange');
-  circle(width/2, height/2, random(300,500));
-  pop();
+   for (let gx = 0; gx < GRID; gx++) {
+     const cx = gx * CELL + CELL / 2;
+     const cy = gy * CELL + CELL / 2;
 
-  push();
-  blendMode(MULTIPLY);
-  translate(width/2, height/2);
-  fill('pink');
-  rectMode(CENTER);
-  const sz = random(300,500);
-  rect(0, 0, sz, sz);
-  pop();
+     // cy + 270
+
+     push();
+     blendMode(MULTIPLY);
+     fill('orange');
+     circle(cx, cy, cz);
+     pop();
+
+     push();
+     blendMode(MULTIPLY);
+     translate(cx, cy);
+     fill('pink');
+     rectMode(CENTER);
+     rect(0, 0, sz, sz);
+     pop();
+   }
+ }
+}
+
+function keyPressed() {
+  if (key === 's') {
+    saveGif('mySketch', 5);
+  }
 }
